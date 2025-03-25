31dd6986394e0857c2a0cf686b97028947859daf
Akshaygore1 <akshaygore2301@gmail.com>
2025-03-26 00:23:57 +0530

    added env example

--- server/config.ts
+++ server/config.ts
@@ -4,6 +4,5 @@ dotenv.config();
 export const Config = {
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   PORT: process.env.PORT,
-  GOOGLE_API_KEY: process.env.GEMINI_KEY as string,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY as string,
 };
